0e6d8cbb17d227b083570d4c3575f5fecaaf7ed3
Alexandra Hurst <82131699+AlNHurst@users.noreply.github.com>
2021-06-13 21:48:08 -0400

    created array of objects for tables and waitlist; set routes to display JSON and html pages

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 // express.json allows you to take in req.body
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Hot Restaurant!');
-});
-
-const dummyArray = [
+const tables = [
     {
-        table: 1,
-        id: 55,
-        name: "Alexandra",
-        email: "fake@thefakest.com",
-        phone: 7041234567,
+        id: "AH768",
+        name: "Alice Springs",
+        email: "test@test.com",
+        phoneNumber: "704-123-4567",
     },
     {
-        table: 2,
-        id: 56,
-        name: "Alexa",
-        email: "faker@thefakest.com",
-        phone: 7041234568,
-    },
-    {
-        table: 3,
-        id: 57,
-        name: "Alex",
-        email: "fakest@thefakest.com",
-        phone: 7041234569,
-    },
+        id: "DT518",
+        name: "Derrick Thomas",
+        email: "test1@test.com",
+        phoneNumber: "704-765-4321",
+    }
 ];
+
+const waitlist = [
+    {
+        id: "OK518",
+        name: "Olivia Kirk",
+        email: "test1@test.com",
+        phoneNumber: "704-951-7538",
+    }
+];
+
+// route to home html
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+// route to tables html
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+// route to reservations html
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+// api data
+app.get('/api/tables', (req, res) => res.json(tables));
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
+
 
 
 app.listen(PORT, () => {
