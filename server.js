e3d54cc0b6c7990fd323a66c36a96f144288bbae
Alexandra Hurst <82131699+AlNHurst@users.noreply.github.com>
2021-06-13 21:00:17 -0400

    completed phase 2

--- server.js
+++ server.js
@@ -3,11 +3,36 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
+// express.json allows you to take in req.body
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Hot Restaurant!');
 });
+
+const dummyArray = [
+    {
+        table: 1,
+        id: 55,
+        name: "Alexandra",
+        email: "fake@thefakest.com",
+        phone: 7041234567,
+    },
+    {
+        table: 2,
+        id: 56,
+        name: "Alexa",
+        email: "faker@thefakest.com",
+        phone: 7041234568,
+    },
+    {
+        table: 3,
+        id: 57,
+        name: "Alex",
+        email: "fakest@thefakest.com",
+        phone: 7041234569,
+    },
+];
 
 
 app.listen(PORT, () => {
